4d864c8115bb1f51e0c61657226a789b081d3fc0
Alexey <greenbelg@internet.ru>
2024-04-15 21:43:21 +0500

    Work is going

---
+++ Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Renga;
+
+namespace PluginsForRenga
+{
+    public class Plugin : IPlugin
+    {
+        private Application m_app;
+        private List<Renga.ActionEventSource> m_eventSources = new List<Renga.ActionEventSource>();
+
+        public bool Initialize(string pluginFolder)
+        {
+            m_app = new Renga.Application();
+            var ui = m_app.UI;
+            var panelExtension = ui.CreateUIPanelExtension();
+
+            // Tool button:
+            panelExtension.AddToolButton(
+              CreateAction(ui, "Синхронизировать свойства"));
+
+            ui.AddExtensionToPrimaryPanel(panelExtension);
+            ui.AddExtensionToActionsPanel(panelExtension, Renga.ViewType.ViewType_View3D);
+
+            return true;
+        }
+
+        public void Stop()
+        {
+            foreach (var eventSource in m_eventSources)
+                eventSource.Dispose();
+
+            m_eventSources.Clear();
+        }
+
+        private Renga.IAction CreateAction(Renga.IUI ui, string displayName)
+        {
+            var action = ui.CreateAction();
+            action.DisplayName = displayName;
+
+            var events = new Renga.ActionEventSource(action);
+            events.Triggered += (s, e) =>
+            {
+                var a = ui.CreateUIPanelExtension();
+            };
+
+            m_eventSources.Add(events);
+
+            return action;
+        }
+    }
+}
