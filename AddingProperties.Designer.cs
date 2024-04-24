be2951c04a06ac0869f5cdafe8dafaeabd8ea76b
Arseniy Yuzhanin <uzaninarsenij28@gmail.com>
2024-04-24 18:06:49 +0300

    fix for working with tables

--- AddingProperties.Designer.cs
+++ AddingProperties.Designer.cs
@@ -29,40 +29,32 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.ViewFiles = new System.Windows.Forms.Button();
             this.CancelAdding = new System.Windows.Forms.Button();
             this.AddProperties = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(38, 76);
+            this.label1.Location = new System.Drawing.Point(26, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(226, 32);
+            this.label1.Size = new System.Drawing.Size(154, 24);
             this.label1.TabIndex = 0;
             this.label1.Text = "Загрузите файл";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(44, 142);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(569, 52);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
             // ViewFiles
             // 
             this.ViewFiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.ViewFiles.Location = new System.Drawing.Point(675, 142);
+            this.ViewFiles.Location = new System.Drawing.Point(450, 93);
+            this.ViewFiles.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ViewFiles.Name = "ViewFiles";
-            this.ViewFiles.Size = new System.Drawing.Size(167, 52);
+            this.ViewFiles.Size = new System.Drawing.Size(111, 34);
             this.ViewFiles.TabIndex = 2;
             this.ViewFiles.Text = "Обзор";
             this.ViewFiles.UseVisualStyleBackColor = true;
@@ -71,9 +63,10 @@
             // CancelAdding
             // 
             this.CancelAdding.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.CancelAdding.Location = new System.Drawing.Point(704, 628);
+            this.CancelAdding.Location = new System.Drawing.Point(470, 408);
+            this.CancelAdding.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.CancelAdding.Name = "CancelAdding";
-            this.CancelAdding.Size = new System.Drawing.Size(138, 50);
+            this.CancelAdding.Size = new System.Drawing.Size(92, 32);
             this.CancelAdding.TabIndex = 3;
             this.CancelAdding.Text = "Отмена";
             this.CancelAdding.UseVisualStyleBackColor = true;
@@ -82,9 +75,10 @@
             // AddProperties
             // 
             this.AddProperties.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.AddProperties.Location = new System.Drawing.Point(509, 628);
+            this.AddProperties.Location = new System.Drawing.Point(339, 408);
+            this.AddProperties.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.AddProperties.Name = "AddProperties";
-            this.AddProperties.Size = new System.Drawing.Size(164, 50);
+            this.AddProperties.Size = new System.Drawing.Size(110, 32);
             this.AddProperties.TabIndex = 4;
             this.AddProperties.Text = "Добавить";
             this.AddProperties.UseVisualStyleBackColor = true;
@@ -94,36 +88,37 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(40, 235);
+            this.label2.Location = new System.Drawing.Point(27, 153);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(694, 60);
+            this.label2.Size = new System.Drawing.Size(486, 44);
             this.label2.TabIndex = 5;
             this.label2.Text = "Примечание:\r\nДобавятся свойства, отсутствующие в текущем проекте";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
-            // button1
+            // label3
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(312, 628);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(164, 50);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Удалить";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.DeleteClick);
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(30, 93);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(397, 33);
+            this.label3.TabIndex = 6;
             // 
             // AddingProperties
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(873, 703);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(582, 457);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.AddProperties);
             this.Controls.Add(this.CancelAdding);
             this.Controls.Add(this.ViewFiles);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "AddingProperties";
             this.Text = "AddingProperties";
             this.ResumeLayout(false);
@@ -134,11 +129,10 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button ViewFiles;
         private System.Windows.Forms.Button CancelAdding;
         private System.Windows.Forms.Button AddProperties;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
     }
 }
